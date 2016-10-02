bdb194fdcd327baf75a4fca1f35b9deeaae7652c
attakei <attakei@gmail.com>
2016-10-03 02:24:11 +0900

    [WIP] Base of article component

---
+++ resources/assets/typescript/article.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'article',
+    template: `
+    <div id="main-markdown"></div>
+    `
+})
+export class ArticleComponent
+{
+}
